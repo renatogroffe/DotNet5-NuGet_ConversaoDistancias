6c89f2f98b4b20f8f13bb57be9f2cbba5757660d
Renato Groffe <renato.groffe@yahoo.com.br>
2021-01-07 16:51:11 -0300

    .NET 5 + Class Library + Configurações NuGet + Conversão de Distâncias
    .NET 5 + Class Library + Configurações NuGet + Conversão de Distâncias

---
+++ Groffe.Distancias/ConversorDistancias.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Groffe.Distancias
+{
+    public static class ConversorDistancias
+    {
+        public static double MilhasParaKm(double milhas) =>
+            Math.Round(Convert.ToDouble(milhas) * 1.609, 3);
+    }
+}
